2efd1f06f9d3a6533d177a04a5136fe7678be7d9
xiaoping <1693547683@qq.com>
2022-03-21 12:45:41 +0800

    init

--- user/src/test/java/com/example/user/UserApplicationTests.java
+++ user/src/test/java/com/example/user/UserApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class UserApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @Test
+    public void set(){
+//        redisTemplate.opsForValue().set("myKey","myValue");
+        long num = 100000;
+        for (int i = 1;i<10000;i++){
+            num+=i;
+            redisTemplate.opsForValue().set("candy_"+num,"唐棠_"+num);
+        }
+
+//        redisTemplate.opsForHash().("myKey","myValue");
+        System.out.println(redisTemplate.opsForValue().get("sunday"));
     }
 
 }
